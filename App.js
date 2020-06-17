42da0e1adcbc850d77e620dfdf187c691ca2e3f1
Phoebe0524 <63526084+Phoebe0524@users.noreply.github.com>
2020-06-17 10:53:15 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import Button from "./app/components/Button";
+
+export default function App() {
+  return <WelcomeScreen />;
+}
+
+// <WelcomeScreen />
+//<Button title="Login" />
---
+++ app/components/Button.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
+function Button({ title, onPress }) {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#fc5c65",
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 15,
+    width: "100%",
+  },
+  text: {
+    color: "#fff",
+    fontSize: 18,
+    textTransform: "uppercase",
+    fontWeight: "bold",
+  },
+});
+
+export default Button;
