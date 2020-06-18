661a4bd25e1c4a401761e89100b33b8727b79ad5
Phoebe0524 <63526084+Phoebe0524@users.noreply.github.com>
2020-06-18 18:46:14 -0400

    modifying

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
-import * as React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import TopTab from './app/components/Tab';
+import * as React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import TopTab from "./app/components/Tab";
 
 export default function App() {
-  return (
-    <TopTab />
-  );
+  return <TopTab />;
 }
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,70 +1,5 @@
-{/*
-import React, { Component } from "react";
-import {Link, View, ImageBackground, StyleSheet, Text, TextInput} from "react-native";
-import Button from "../components/Button";
-import { LoginButton } from 'react-native-fbsdk';
-
-
-class FBLoginButton extends Component {
-  render() {
-    return (
-      <View>
-        <LoginButton
-          publishPermissions={["email"]}
-          onLoginFinished={
-            (error, result) => {
-              if (error) {
-                alert("Login failed with error: " + error.message);
-              } else if (result.isCancelled) {
-                alert("Login was cancelled");
-              } else {
-                alert("Login was successful with permissions: " + result.grantedPermissions)
-              }
-            }
-          }
-          onLogoutFinished={() => alert("User logged out")}/>
-      </View>
-    );
-  }
-};
-
-
-export default class LoginScreen extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.label}>Welcome to the Facebook SDK for React Native!</Text>
-        <FBLoginButton />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  label: {
-    fontSize: 16,
-    fontWeight: 'normal',
-    marginBottom: 48,
-  },
-});
-
-//AppRegistry.registerComponent('YourApp', () => YourApp);
-
-*/}
-
-
-
-
-
 import React from "react";
-import { Link, View, ImageBackground, StyleSheet, Text} from "react-native";
+import { Link, View, ImageBackground, StyleSheet, Text } from "react-native";
 import Button from "../components/Button";
 
 export default function WelcomeScreen(props) {
@@ -78,7 +13,6 @@ export default function WelcomeScreen(props) {
         <Button title="Login" />
       </View>
     </ImageBackground>
-
   );
 }
 
